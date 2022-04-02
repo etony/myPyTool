8543d876ef8e7a4dab3f570ed79fff1cad7ac26b
etony <etony.an@gmail.com>
2022-04-02 11:06:00 +0800

    update

--- 炸金花.py
+++ 炸金花.py
@@ -20,57 +20,63 @@ Created on Mon Feb 14 15:16:03 2022
 
 '''
 
-import random,collections
-colors = ["红桃","黑桃","方块","梅花"] #牌色
+import random
+import collections
+colors = ["红桃", "黑桃", "方块", "梅花"]  # 牌色
 
-digi =  [str(i) for i in range(2,11)] + ['J','Q','K','A'] # 点数
+digi = [str(i) for i in range(2, 11)] + ['J', 'Q', 'K', 'A']  # 点数
 
-n =5 # 玩家
+n = 5  # 玩家
 
 
-#返回项 info = {”牌型":,"分数":}  
-    
-def getscore(pk_lst): # 检查大小
+# 返回项 info = {”牌型":,"分数":}
+
+def getscore(pk_lst):  # 检查大小
     # print(pk_lst,end="  ")
-    colors = len(set(pk_color[:2] for pk_color in pk_lst)) # 是否同色 
-    
-    lst = list(sc[2:] for sc in pk_lst) #转换分值
+    colors = len(set(pk_color[:2] for pk_color in pk_lst))  # 是否同色
+
+    lst = list(sc[2:] for sc in pk_lst)  # 转换分值
     for i in range(3):
-        if lst[i] == "A" :lst[i] =14
-        elif lst[i] == "J" :lst[i] =11
-        elif lst[i] == "Q" :lst[i] =12
-        elif lst[i] == "K" :lst[i] =13
-        else : lst[i] = int(lst[i])
+        if lst[i] == "A":
+            lst[i] = 14
+        elif lst[i] == "J":
+            lst[i] = 11
+        elif lst[i] == "Q":
+            lst[i] = 12
+        elif lst[i] == "K":
+            lst[i] = 13
+        else:
+            lst[i] = int(lst[i])
 
     info = {}
-    info['score']=0
-    if (len(set(lst))==1):
-        info['type']="豹子"
-        info['score']=max(lst)*10**6
-        
-    if len(set(lst))==2:
+    info['score'] = 0
+    if (len(set(lst)) == 1):
+        info['type'] = "豹子"
+        info['score'] = max(lst)*10**6
+
+    if len(set(lst)) == 2:
         count = collections.Counter(lst)
         for i in count:
             if count[i] == 2:
-                info['type']="对子"
-                info['score']=i*10**2 + info['score']
+                info['type'] = "对子"
+                info['score'] = i*10**2 + info['score']
             if count[i] == 1:
-                info['score']=i + info['score']
-    if len(set(lst)) == 3 :
-        if max(lst) == min(lst)+2 or set(lst)=={2,3,14}:
-            info['type']="顺子"
-            info['score']=max(lst)*10**3
-            if colors ==1:
-                info['type']="金顺"
-                info['score']=max(lst)*10**5              
+                info['score'] = i + info['score']
+    if len(set(lst)) == 3:
+        if max(lst) == min(lst)+2 or set(lst) == {2, 3, 14}:
+            info['type'] = "顺子"
+            info['score'] = max(lst)*10**3
+            if colors == 1:
+                info['type'] = "金顺"
+                info['score'] = max(lst)*10**5
 
         else:
-            if colors ==1 :
-                info['type']="金花"
-                info['score']=max(lst)*10**4
+            if colors == 1:
+                info['type'] = "金花"
+                info['score'] = max(lst)*10**4
             else:
-                info['type']="单张"
-                info['score']=max(lst)
+                info['type'] = "单张"
+                info['score'] = max(lst)
 
     print(info)
     return info
@@ -82,41 +88,34 @@ def get_pk_lst():
         for di in digi:
             pk.append(col+di)
 
-
-    player_list =[]
-    for i in range(1,n+1):
+    player_list = []
+    for i in range(1, n+1):
         player_list.append('熊-'+str(i))
 
-
-    pk_list={}
+    pk_list = {}
 
     for i in player_list:
-        pk_list[i]=[]
-        pk_list[i]=(random.sample(pk, 3))
+        pk_list[i] = []
+        pk_list[i] = (random.sample(pk, 3))
         for p in pk_list[i]:
             pk.remove(p)
-            
-    
-    return pk_list
-    
-    
-if __name__ == '__main__':     
 
-    pk_list = get_pk_lst()         
-    
+    return pk_list
+
+
+if __name__ == '__main__':
+
+    pk_list = get_pk_lst()
+
     print("发牌-----")
     result = {}
-    for player,pk_lst in pk_list.items():
+    for player, pk_lst in pk_list.items():
         print(f'{player:<5}{str(pk_lst):<23}', end="      ")
         info = getscore(pk_list[player])
         result[player] = info['score']
     print(pk_list)
     print("\n比较-----")
-    
+
     for key, value in result.items():
         if value == max(result.values()):
             print(key + " 胜 ")
-    
-
-        
-    
