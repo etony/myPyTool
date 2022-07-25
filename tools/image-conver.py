cd194b6c5d086bd31d244df337aee05075031a73
etony <etony.an@gmail.com>
2022-07-25 15:50:24 +0800

    update image-conver

--- tools/image-conver.py
+++ tools/image-conver.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
-
 """
 Module implementing Image-Conver.
 """
 
 from PyQt6.QtCore import pyqtSlot, Qt
 from PyQt6.QtWidgets import QMainWindow, QFileDialog
-
 
 from Ui_imageconver import Ui_MainWindow
 from PyQt6 import QtCore, QtGui, QtWidgets
@@ -16,11 +14,11 @@ from torchvision.transforms.functional import to_tensor, to_pil_image
 from PIL import Image
 
 
-
 class Image_Conver(QMainWindow, Ui_MainWindow):
     """
     Class documentation goes here.
     """
+
     def __init__(self, parent=None):
         """
         Constructor
@@ -32,7 +30,7 @@ class Image_Conver(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.pbstart.setDisabled(True)
         self.pbsave.setDisabled(True)
-    
+
     @pyqtSlot()
     def on_pbselect_clicked(self):
         """
@@ -40,7 +38,8 @@ class Image_Conver(QMainWindow, Ui_MainWindow):
         """
         # TODO: not implemented yet
         global imgNamepath
-        imgNamepath,  imgType = QFileDialog.getOpenFileName(self, "选择图片", "D:\\", "*.jpg;;*.png;;All Files(*)")
+        imgNamepath, imgType = QFileDialog.getOpenFileName(
+            self, "选择图片", "D:\\", "*.jpg;;*.png;;All Files(*)")
         self.label_2.setScaledContents(True)
         img = QtGui.QPixmap(imgNamepath).scaled(self.label_2.size())
         print("img: ", img.width(), img.height())
@@ -51,9 +50,7 @@ class Image_Conver(QMainWindow, Ui_MainWindow):
         # 显示所选图片的路径
         self.lineEdit_2.setText(imgNamepath)
         self.pbstart.setEnabled(True)
-        
-        
-        
+
     @pyqtSlot()
     def on_pbstart_clicked(self):
         """
@@ -62,11 +59,12 @@ class Image_Conver(QMainWindow, Ui_MainWindow):
         self.label.setText("开始加载图片.......")
         # TODO: not implemented yet
         net = Generator()
-        net.load_state_dict(torch.load("./torch/face_paint_512_v2.pt", map_location="cpu"))
+        net.load_state_dict(
+            torch.load("./torch/face_paint_512_v2.pt", map_location="cpu"))
         net.to("cpu").eval()
         image = self.load_image(imgNamepath)
         print("开始加载图片.......")
-        
+
         with torch.no_grad():
             image = to_tensor(image).unsqueeze(0) * 2 - 1
             out = net(image.to("cpu"), False).cpu()
@@ -75,13 +73,14 @@ class Image_Conver(QMainWindow, Ui_MainWindow):
         image_name = imgNamepath.split(".")[0]
         out.save(image_name + "_animegan" + ".png")
         print("图片保存成功！！")
-        imgShow = QtGui.QPixmap(image_name + "_animegan" + ".png").scaled(self.label.size())
+        imgShow = QtGui.QPixmap(image_name + "_animegan" + ".png").scaled(
+            self.label.size())
         self.label.setFixedSize(imgShow.width(), imgShow.height())
         self.label.setScaledContents(True)
         self.label.setPixmap(imgShow)
         print(f"image saved: {image_name}")
         self.pbsave.setEnabled(True)
-        
+
     @pyqtSlot()
     def on_pbsave_clicked(self):
         """
@@ -89,17 +88,21 @@ class Image_Conver(QMainWindow, Ui_MainWindow):
         """
         # TODO: not implemented yet
         img = self.label.pixmap().toImage()
-        fpath, ftype = QFileDialog.getSaveFileName(self, "保存", "D:\\", "*.jpg;;*.png;;All Files(*)")
+        fpath, ftype = QFileDialog.getSaveFileName(
+            self, "保存", "D:\\", "*.jpg;;*.png;;All Files(*)")
         img.save(fpath)
-        
+
     def load_image(self, image_path, x32=False):
         img = Image.open(image_path).convert("RGB")
         if x32:
+
             def to_32s(x):
                 return 256 if x < 256 else x - x % 32
+
             w, h = img.size
             img = img.resize((to_32s(w), to_32s(h)))
         return img
+
 
 if __name__ == "__main__":
     import sys
