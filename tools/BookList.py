56266a2d261bcec1f7371d8795b24f8d0cd19da9
etony <etony.an@gmail.com>
2023-06-10 19:16:26 +0800

    update

--- tools/BookList.py
+++ tools/BookList.py
@@ -19,6 +19,7 @@ import pyzbar.pyzbar as pyzbar
 import requests
 import json
 
+
 class TableModel(QtCore.QAbstractTableModel):
 
     def __init__(self, data):
@@ -30,21 +31,21 @@ class TableModel(QtCore.QAbstractTableModel):
             value = self._data.iloc[index.row(), index.column()]
             return str(value)
 
-    def rowCount(self, parent=None):#index):
+    def rowCount(self, parent=None):  # index):
         return self._data.shape[0]
-    
+
     def appendRow(self, arowdata):
         self.beginResetModel()
-        self._data.loc[self._data.shape[0]]=arowdata
+        self._data.loc[self._data.shape[0]] = arowdata
         self.endResetModel()
 
     def updateData(self, data):
 
         self.beginResetModel()
         self._data = data
-        self.endResetModel()        
-        
-    def columnCount(self, parent=None):#index):
+        self.endResetModel()
+
+    def columnCount(self, parent=None):  # index):
         return self._data.shape[1]
 
     def headerData(self, section, orientation, role):
@@ -55,12 +56,12 @@ class TableModel(QtCore.QAbstractTableModel):
 
             if orientation == Qt.Orientation.Vertical:
                 return str(self._data.index[section])
+
     def clear(self):
         self._data.drop(self._data.index, inplace=True)
-        
+
     def dataexport(self):
         return self._data
-
 
 
 class BLmainWindow(QMainWindow, Ui_mainWindow):
@@ -77,20 +78,19 @@ class BLmainWindow(QMainWindow, Ui_mainWindow):
         """
         super().__init__(parent)
         self.setupUi(self)
-        data = {'ISBN':[], '书名':[], '作者':[], '出版社':[], '价格':[], '分类':[], '书柜':[]}
-        
+        data = {'ISBN': [], '书名': [], '作者': [],
+                '出版社': [], '价格': [], '分类': [], '书柜': []}
+
         df = pd.DataFrame(data=data)
-        
+
         self.model = TableModel(df)
         self.tv_booklist.setModel(self.model)
         # self.model = QtGui.QStandardItemModel()
         # self.model.setHorizontalHeaderLabels(
-        #     ['ISBN', '书名', '作者', '出版社', '价格', '分类', '书柜']) 
+        #     ['ISBN', '书名', '作者', '出版社', '价格', '分类', '书柜'])
 
         # self.tv_booklist.setModel(self.model)
         self.tv_booklist.setSortingEnabled(True)
-        
-     
 
     @pyqtSlot()
     def on_pb_load_clicked(self):
@@ -103,14 +103,11 @@ class BLmainWindow(QMainWindow, Ui_mainWindow):
 
         if csvNamepath != "":
             df = pd.read_csv(csvNamepath)
- 
+
             self.model = TableModel(df)
-  
 
             self.tv_booklist.setModel(self.model)
-            #self.table.setModel(self.model)
-             
-
+            # self.table.setModel(self.model)
 
     @pyqtSlot()
     def on_pb_save_clicked(self):
@@ -120,8 +117,8 @@ class BLmainWindow(QMainWindow, Ui_mainWindow):
         # TODO: not implemented yet
         isbn = self.le_isbn_pic.text()
         bookinfo = self.get_douban_isbn(isbn)
-        self.model.appendRow(bookinfo) 
-        
+        self.model.appendRow(bookinfo)
+
         print(bookinfo)
 
     @pyqtSlot()
@@ -134,56 +131,54 @@ class BLmainWindow(QMainWindow, Ui_mainWindow):
             self, "选择条形码图片", "E:\\minipan\\Seafile\\资料", "*.png;;*.jpg;;All Files(*)")
 
         if picNamepath != "":
-            #image = cv.imread(img_path)
-            image = cv.imdecode(np.fromfile(picNamepath,dtype=np.uint8), cv.IMREAD_COLOR)
-            
+            # image = cv.imread(img_path)
+            image = cv.imdecode(np.fromfile(
+                picNamepath, dtype=np.uint8), cv.IMREAD_COLOR)
+
             gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-           
+
             binary, _ = cv.threshold(gray, 0, 255, cv.THRESH_OTSU)
             binary, mat = cv.threshold(gray, binary, 255, cv.THRESH_BINARY)
-            
-            
-            
+
             barcode = pyzbar.decode(mat)
             for bar in barcode:
                 self.le_isbn_pic.setText(bar.data.decode("utf-8"))
-            
+
             # data =['xxxxxx', '西线无战事', '作者: [德] 雷马克 翻译:朱雯', '上海人民出版社', '45.00', '计划', '未设置']
 
             # dd = map(lambda x: QtGui.QStandardItem(x), data)
-            
+
             # data = get_douban_isbn()
             # # self.model.clear()
-            
-            # self.model.appendRow(data) 
-              
+
+            # self.model.appendRow(data)
+
             # self.tv_booklist.setModel()
 
-            #self.model.insertRow(self.model.rowCount(),data)
+            # self.model.insertRow(self.model.rowCount(),data)
 
-            #self.tv_booklist.setModel(self.model)
+            # self.tv_booklist.setModel(self.model)
 
-
-
-    def get_douban_isbn(self,isbn):
+    def get_douban_isbn(self, isbn):
         bookinfo = []
         url = "https://api.douban.com/v2/book/isbn/" + isbn
 
-        payload = {'apikey':'0ab215a8b1977939201640fa14c66bab'}  #apikey=0df993c66c0c636e29ecbb5344252a4a'} #apikey=0ac44ae016490db2204ce0a042db2916
+        # apikey=0df993c66c0c636e29ecbb5344252a4a'} #apikey=0ac44ae016490db2204ce0a042db2916
+        payload = {'apikey': '0ab215a8b1977939201640fa14c66bab'}
         headers = {"Referer": "https://m.douban.com/tv/american",
                    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"}
 
-        #{"content-type": "multipart/form-data;","User-Agent": "MicroMessenger/","Referer": "https://servicewechat.com/wx2f9b06c1de1ccfca/91/page-frame.html"}
+        # {"content-type": "multipart/form-data;","User-Agent": "MicroMessenger/","Referer": "https://servicewechat.com/wx2f9b06c1de1ccfca/91/page-frame.html"}
 
         response = requests.post(url, data=payload, headers=headers)
 
         book_dict = json.loads(response.text)
-        
+
         author = '作者: ' + '/'.join(book_dict['author'])
-        if len(book_dict['translator'])>0:
+        if len(book_dict['translator']) > 0:
             author += ' 译者: '
             author += '/'.join(book_dict['translator'])
-        
+
         bookinfo.append(isbn)
         bookinfo.append(book_dict['title'])
         bookinfo.append(author)
@@ -193,7 +188,6 @@ class BLmainWindow(QMainWindow, Ui_mainWindow):
         bookinfo.append('未设置')
 
         return bookinfo
-
 
 
 if __name__ == "__main__":
