1cca9b97da465d3736e98dfa2d953ce46513b5ea
etony <etony.an@gmail.com>
2025-06-13 21:10:27 +0800

    update

--- tools/Ui_BooSearch.py
+++ tools/Ui_BooSearch.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
+    ui = Ui_Dialog_S()
     ui.setupUi(Dialog)
     Dialog.show()
     sys.exit(app.exec())
